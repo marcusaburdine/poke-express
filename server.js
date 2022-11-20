28438e1d30efc273d42510097796a1afd449e90b
marcusaburdine <113939789+marcusaburdine@users.noreply.github.com>
2022-11-20 15:22:24 -0500

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -11,14 +11,21 @@ const PORT = 3000
 
 const pokemon = require("./models/pokemon")
 
+
+
+app.set("view engine", "jsx")
+app.engine("jsx", require("express-react-views").createEngine())
+
+
 app.get("/", (req, res) => {
    
     res.send("Welcome to the Pokemon App! ")
   })
 
+
   app.get("/pokemon", (req, res) => {
    
-    res.send(pokemon)
+    res.render("Index")
   })
 
 app.listen(PORT, () => {
---
+++ views/Index.jsx
@@ -0,0 +1,14 @@
+const React = require("react")
+class Index extends React.Component {
+    render() {
+       
+        return (
+            <div>
+                <h1>Pokemon Index Page</h1>
+               
+            </div>
+        )
+    }
+}
+
+module.exports = Index
