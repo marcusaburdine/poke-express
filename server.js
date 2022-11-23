dff8b9e239330b2153ec51dbc12c8fcaf9d38637
marcusaburdine <113939789+marcusaburdine@users.noreply.github.com>
2022-11-22 21:39:48 -0500

    Added dynamic anchor tags to index.jsx 01~

--- server.js
+++ server.js
@@ -19,22 +19,17 @@ app.engine("jsx", require("express-react-views").createEngine())
 
 
 app.get("/", (req, res) => {
-   
-    res.send("Welcome to the Pokemon App! ")
-  })
+  res.send("Welcome to the Pokemon App! ")
+})
 
-  app.get("/pokemon", (req, res) => {
-   
-    res.render("Index", { pokemon: pokemon })
-  })
+app.get("/pokemon", (req, res) => {
+  res.render("Index", { pokemon: pokemon })
+})
 
-  app.get("/pokemon/:id", (req, res) => {
-   
-    res.send(`${req.params.id}`)
-  })
-
-
+app.get("/pokemon/:id", (req, res) => {
+  res.send(`${req.params.id}`)
+})
 
 app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`)
-  }) 
+  console.log(`Listening on port: ${PORT}`)
+}) 
